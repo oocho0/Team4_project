cede297c7b6802c5f7616a5493feaa756d636562
USJapanlikeme <Uniquepureart@gmail.com>
2023-02-03 16:23:18 +0900

    button

---
+++ Cloud/button01/button1/js/script.js
@@ -0,0 +1,19 @@
+$("#myButton").click(function(e) {
+  $(this).addClass("clicked");
+});
+
+$("#myButton2").click(function(e) {
+  $(this).addClass("clicked");
+});
+
+$("#myButton3").click(function(e) {
+  $(this).addClass("clicked");
+});
+
+$("#myButton4").click(function(e) {
+  $(this).addClass("clicked");
+});
+
+$("#myButton5").click(function(e) {
+  $(this).addClass("clicked");
+});
