12645bd0be4b25db3fab3ac21010321c1abebc50
Seul ki <Uniquepureart@gmail.com>
2023-02-07 15:56:35 +0900

    Delete Cloud/front_all_1 directory
    delete

--- Cloud/front_all_1/js/menubar.js
+++
@@ -1,7 +0,0 @@
-
-const menuBar = document.querySelector('.menu-bar');
-const navBar = document.querySelector('.nav-bar');
-
-menuBar.addEventListener('click', () => {
-  navBar.classList.toggle('toggle');
-});
